f33ae56dfb203cf264945bf77c6764b5d52f279f
Akira Durham <akiradurham@yahoo.com>
2023-12-04 16:45:42 -0500

    setup Course Review Page

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -1,11 +1,36 @@
 package edu.virginia.sde.reviews;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CourseSearchController {
+    @FXML
+    public TextField courseSubject;
+    @FXML
+    public TextField courseNumber;
+    @FXML
+    public TextField courseTitle;
     private Stage primaryStage;
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
+    }
+
+    @FXML
+    public void search(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void addCourse(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void logout(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void gotoReviews(ActionEvent actionEvent) {
     }
 }
