08104d62993d617413e79078212064771259b6d2
Akira Durham <akiradurham@yahoo.com>
2023-12-04 17:20:19 -0500

    adding validation methods for adding course

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -2,8 +2,11 @@ package edu.virginia.sde.reviews;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class CourseSearchController {
     @FXML
@@ -12,25 +15,80 @@ public class CourseSearchController {
     public TextField courseNumber;
     @FXML
     public TextField courseTitle;
+    @FXML
+    public Label errorLabel;
     private Stage primaryStage;
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
 
-    @FXML
-    public void search(ActionEvent actionEvent) {
+    private void updateTable(){
+
     }
 
     @FXML
-    public void addCourse(ActionEvent actionEvent) {
+    private void search(ActionEvent actionEvent) {
+        errorLabel.setVisible(false);
     }
 
     @FXML
-    public void logout(ActionEvent actionEvent) {
+    private void addCourse(ActionEvent actionEvent) {
+        errorLabel.setVisible(false);
+        if (validateSubject(courseSubject.getText()) && validateNumber(courseNumber.getText()) && validateTitle(courseTitle.getText())){
+
+        }
+    }
+
+    private boolean validateSubject(String subject){
+        if (subject.length() <= 4 && subject.length() >= 2){
+            for (int i = 0; i < subject.length(); i++) {
+                if(!Character.isLetter(subject.charAt(i))) {
+                    errorLabel.setText("The subject is not all letters");
+                    errorLabel.setVisible(true);
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            errorLabel.setText("The subject is incorrect, has to be 2-4 letters");
+            errorLabel.setVisible(true);
+            return false;
+        }
+    }
+
+    private boolean validateNumber(String number){
+        if (number.length() == 4){
+            for (int i = 0; i < 4; i++) {
+                if(!Character.isDigit(number.charAt(i))) {
+                    errorLabel.setText("The number is not all digits");
+                    errorLabel.setVisible(true);
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            errorLabel.setText("The number is too long, has to be 4 digits");
+            errorLabel.setVisible(true);
+            return false;
+        }
+    }
+
+    private boolean validateTitle(String title){
+        if (title.length() >= 1 && title.length() <= 50){
+            return true;
+        } else {
+            errorLabel.setText("The title is incorrect, title has to be from 1 to 50 characters");
+            errorLabel.setVisible(true);
+            return false;
+        }
     }
 
     @FXML
-    public void gotoReviews(ActionEvent actionEvent) {
+    private void logout(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void gotoReviews(ActionEvent actionEvent) {
     }
 }
